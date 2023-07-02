801250477ba1195345fb1057ecd13b08f1ee8b04
ChambersJarod <Jarod.Chambers-Genge@keyin.com>
2023-07-02 21:14:35 -0230

    streamlined the airport folder

--- Sem4_Sprint1Two_Main/src/main/java/com/keyin/airport/Airport.java
+++ Sem4_Sprint1Two_Main/src/main/java/com/keyin/airport/Airport.java
@@ -1,3 +1,35 @@
+//package com.keyin.airport;
+//
+//public class Airport {
+//    private int id;
+//    private String name;
+//    private int cityId;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getCityId() {
+//        return cityId;
+//    }
+//
+//    public void setCityId(int cityId) {
+//        this.cityId = cityId;
+//    }
+//}
+
 package com.keyin.airport;
 
 public class Airport {
@@ -5,27 +37,21 @@ public class Airport {
     private String name;
     private int cityId;
 
-    public int getId() {
-        return id;
+    public Airport(int id, String name, int cityId) {
+        this.id = id;
+        this.name = name;
+        this.cityId = cityId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getCityId() {
         return cityId;
-    }
-
-    public void setCityId(int cityId) {
-        this.cityId = cityId;
     }
 }
