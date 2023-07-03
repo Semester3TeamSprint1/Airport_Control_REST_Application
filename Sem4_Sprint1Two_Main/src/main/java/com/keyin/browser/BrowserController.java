83e15576d83333005b0bfdb66f5b5fe1dc8ac24d
ChambersJarod <Jarod.Chambers-Genge@keyin.com>
2023-07-03 02:23:49 -0230

    removed old code

--- Sem4_Sprint1Two_Main/src/main/java/com/keyin/browser/BrowserController.java
+++ Sem4_Sprint1Two_Main/src/main/java/com/keyin/browser/BrowserController.java
@@ -1,39 +1,3 @@
-//package com.keyin.browser;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//
-//import java.util.List;
-//
-//@RestController
-//@CrossOrigin
-//public class BrowserController {
-//    @Autowired
-//    private BrowserService browserService;
-//
-//    @GetMapping("/browser")
-//    public Browser getPeekBrowser() {
-//        return browserService.getPeekBrowser();
-//    }
-//
-//    @GetMapping("/allBrowserData")
-//    public List<Browser> getAllBrowserData(){
-//        return browserService.getBrowserData();
-//    }
-//
-//    @GetMapping("/undoBrowser")
-//    public Browser undoBrowser(){
-//        return browserService.undoBrowser();
-//    }
-//
-//    @GetMapping("/redoBrowser")
-//    public Browser redoBrowser(){
-//        return browserService.redoBrowser();
-//    }
-//}
 package com.keyin.browser;
 
 import org.springframework.beans.factory.annotation.Autowired;
