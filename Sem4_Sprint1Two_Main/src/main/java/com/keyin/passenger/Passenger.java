d3b488d13d9fdee762f3ef82e8d43731d90c0e6d
ChambersJarod <Jarod.Chambers-Genge@keyin.com>
2023-07-02 23:22:21 -0230

    passenger

--- Sem4_Sprint1Two_Main/src/main/java/com/keyin/passenger/Passenger.java
+++ Sem4_Sprint1Two_Main/src/main/java/com/keyin/passenger/Passenger.java
@@ -1,14 +1,103 @@
+//package com.keyin.passenger;
+//
+//import com.keyin.airport.Airport;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Objects;
+//
+//public class Passenger {
+//    private int id;
+//    private String firstname;
+//    private String lastName;
+//    private String phoneNumber;
+//    private List<Integer> aircraftIdsList = new ArrayList<>();
+//    private List<Integer> airportIdsList = new ArrayList<>();
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstname() {
+//        return firstname;
+//    }
+//
+//    public void setFirstname(String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public List<Integer> getAircraftIdsList() {
+//        return aircraftIdsList;
+//    }
+//
+//    public List<Integer> getAirportIdsList() {
+//        return airportIdsList;
+//    }
+//
+//    public void setAircraftIdsList(List<Integer> aircraftIdsList) {
+//        this.aircraftIdsList = aircraftIdsList;
+//    }
+//
+//    public void setAirportIdsList(List<Integer> airportIdsList) {
+//        this.airportIdsList = airportIdsList;
+//    }
+//
+//    public void addToAircraftIdsList(int id){
+//        aircraftIdsList.add(id);
+//    }
+//
+//    public void removeFromAircraftIdsList(int id){
+//        for (Integer match : aircraftIdsList) {
+//            if(match == id){
+//                aircraftIdsList.remove(match);
+//                System.out.println("The aircraft has been removed from the list");
+//            }
+//        }
+//    }
+//
+//    public void addToAirportIdsList(int id){
+//        airportIdsList.add(id);
+//    }
+//
+//    // used in service yet
+//    public void removeFromAirportIdsList(int id){
+//        for (Integer match : airportIdsList) {
+//            if(match == id){
+//                airportIdsList.remove(match);
+//                System.out.println("The airport has been removed from the list");
+//            }
+//        }
+//    }
+//
+//}
 package com.keyin.passenger;
-
-import com.keyin.airport.Airport;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Passenger {
     private int id;
-    private String firstname;
+    private String firstName;
     private String lastName;
     private String phoneNumber;
     private List<Integer> aircraftIdsList = new ArrayList<>();
@@ -22,12 +111,12 @@ public class Passenger {
         this.id = id;
     }
 
-    public String getFirstname() {
-        return firstname;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
@@ -54,39 +143,21 @@ public class Passenger {
         return airportIdsList;
     }
 
-    public void setAircraftIdsList(List<Integer> aircraftIdsList) {
-        this.aircraftIdsList = aircraftIdsList;
-    }
-
-    public void setAirportIdsList(List<Integer> airportIdsList) {
-        this.airportIdsList = airportIdsList;
-    }
-
-    public void addToAircraftIdsList(int id){
+    public void addToAircraftIdsList(int id) {
         aircraftIdsList.add(id);
     }
 
-    public void removeFromAircraftIdsList(int id){
-        for (Integer match : aircraftIdsList) {
-            if(match == id){
-                aircraftIdsList.remove(match);
-                System.out.println("The aircraft has been removed from the list");
-            }
-        }
+    public void removeFromAircraftIdsList(int id) {
+        aircraftIdsList.removeIf(match -> match == id);
+        System.out.println("removed from list");
     }
 
-    public void addToAirportIdsList(int id){
+    public void addToAirportIdsList(int id) {
         airportIdsList.add(id);
     }
 
-    // used in service yet
-    public void removeFromAirportIdsList(int id){
-        for (Integer match : airportIdsList) {
-            if(match == id){
-                airportIdsList.remove(match);
-                System.out.println("The airport has been removed from the list");
-            }
-        }
+    public void removeFromAirportIdsList(int id) {
+        airportIdsList.removeIf(match -> match == id);
+        System.out.println("removed from list");
     }
-
 }
--- Sem4_Sprint1Two_Main/src/main/java/com/keyin/passenger/PassengerController.java
+++ Sem4_Sprint1Two_Main/src/main/java/com/keyin/passenger/PassengerController.java
@@ -1,5 +1,104 @@
+//package com.keyin.passenger;
+//
+//
+//import com.keyin.activity.ActivityService;
+//import com.keyin.aircraft.Aircraft;
+//import com.keyin.airport.Airport;
+//import com.keyin.browser.BrowserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//import java.util.Map;
+//
+//@RestController
+//@CrossOrigin
+//public class PassengerController {
+//    @Autowired
+//    private PassengerService passengerService;
+//    @Autowired
+//    private BrowserService browserService;
+//    @Autowired
+//    private ActivityService activityService;
+//
+//    @GetMapping("/passenger")
+//    public List<Passenger> getAllPassenger() {
+//        browserService.addToBrowser("getAllPassenger()", "/passenger", LocalDateTime.now());
+//        return passengerService.getAllPassenger();
+//    }
+//
+//    @GetMapping("/passenger/{id}")
+//    public Passenger getPassengerById(@PathVariable int id) {
+//        String url = "/passenger/" + String.valueOf(id);
+//        browserService.addToBrowser("getPassengerById()", url, LocalDateTime.now());
+//        return passengerService.getPassengerById(id);
+//    }
+//
+//    @GetMapping("/passenger/search")
+//    public List<Passenger> searchPassenger(@RequestParam String toSearch){
+//        browserService.addToBrowser("searchPassenger()", "passenger/search", LocalDateTime.now());
+//        return passengerService.searchPassenger(toSearch);
+//    }
+//
+//    @PostMapping("/passenger/addPassenger")
+//    public void addPassenger(@RequestBody Passenger passenger){
+//        activityService.addActivity("passenger", "create", Map.of("id", passenger.getId(), "firstname",  passenger.getFirstname(), "lastName", passenger.getLastName(), "phoneNumber", passenger.getPhoneNumber(), "aircraftIdsList", passenger.getAircraftIdsList(), "airportIdsList", passenger.getAirportIdsList()));
+//        browserService.addToBrowser("addPassenger()", "/passenger/addPassenger", LocalDateTime.now());
+//        passengerService.addPassenger(passenger);
+//    }
+//
+//    @DeleteMapping("/passenger/deletePassenger/{id}")
+//    public List<Passenger> deletePassengerById(@PathVariable int id) {
+//        Passenger passengerForActivity = new Passenger();
+//        List<Passenger> passengerlist = passengerService.getAllPassenger();
+//        for (Passenger passenger : passengerlist){
+//            if (passenger.getId() == id) {
+//                passengerForActivity = passenger;
+//            }
+//        }
+//        if (passengerForActivity != null) {
+//            activityService.addActivity("passenger", "delete", Map.of("id", passengerForActivity.getId(), "firstname",  passengerForActivity.getFirstname(), "lastName", passengerForActivity.getLastName(), "phoneNumber", passengerForActivity.getPhoneNumber(), "aircraftIdsList", passengerForActivity.getAircraftIdsList(), "airportIdsList", passengerForActivity.getAirportIdsList()));
+//        }
+//
+//        String url = "/passenger/deletePassenger/" + String.valueOf(id);
+//        browserService.addToBrowser("deletePassenger()", url, LocalDateTime.now());
+//        return passengerService.deletePassengerById(id);
+//    }
+//
+//    @PutMapping("/passenger/updatePassenger/{id}")
+//    public List<Passenger> updatePassenger(@PathVariable int id, @RequestBody Passenger passenger){
+//        Passenger passengerForActivity = new Passenger();
+//        List<Passenger> passengerlist = passengerService.getAllPassenger();
+//        for (Passenger passengerToFind : passengerlist){
+//            if (passengerToFind.getId() == id) {
+//                passengerForActivity = passengerToFind;
+//            }
+//        }
+//        if (passengerForActivity != null) {
+//            activityService.addActivity("passenger", "update", Map.of("id", passengerForActivity.getId(), "firstname",  passengerForActivity.getFirstname(), "lastName", passengerForActivity.getLastName(), "phoneNumber", passengerForActivity.getPhoneNumber(), "aircraftIdsList", passengerForActivity.getAircraftIdsList(), "airportIdsList", passengerForActivity.getAirportIdsList()));
+//        }
+//
+//        String url = "/passenger/updatePassenger/" + String.valueOf(id);
+//        browserService.addToBrowser("updatePassenger()", url, LocalDateTime.now());
+//        return passengerService.updatePassenger(id, passenger);
+//    }
+//
+//    @GetMapping("/passenger/{id}/getAircraft")
+//    public List<Aircraft> getAircraftPassengerTravelledOn(@PathVariable int id) {
+//        String url = "/passenger/"  + String.valueOf(id) + "/getAircraft";
+//        browserService.addToBrowser("getAircraft()", url, LocalDateTime.now());
+//        return passengerService.getAircraft(id);
+//    }
+//
+//    @GetMapping("/passenger/{id}/getAirport")
+//    public List<Airport> getAirportPassengerVisited(@PathVariable int id) {
+//        String url = "/passenger/"  + String.valueOf(id) + "/getAirport";
+//        browserService.addToBrowser("getAirport()", url, LocalDateTime.now());
+//        return passengerService.getAirports(id);
+//    }
+//}
 package com.keyin.passenger;
-
 
 import com.keyin.activity.ActivityService;
 import com.keyin.aircraft.Aircraft;
@@ -23,78 +122,63 @@ public class PassengerController {
     private ActivityService activityService;
 
     @GetMapping("/passenger")
-    public List<Passenger> getAllPassenger() {
-        browserService.addToBrowser("getAllPassenger()", "/passenger", LocalDateTime.now());
-        return passengerService.getAllPassenger();
+    public List<Passenger> getAllPassengers() {
+        return performOperation("getAllPassengers()", "/passenger", () -> passengerService.getAllPassengers());
     }
 
     @GetMapping("/passenger/{id}")
     public Passenger getPassengerById(@PathVariable int id) {
-        String url = "/passenger/" + String.valueOf(id);
-        browserService.addToBrowser("getPassengerById()", url, LocalDateTime.now());
-        return passengerService.getPassengerById(id);
+        String url = "/passenger/" + id;
+        return performOperation("getPassengerById()", url, () -> passengerService.getPassengerById(id));
     }
 
     @GetMapping("/passenger/search")
-    public List<Passenger> searchPassenger(@RequestParam String toSearch){
-        browserService.addToBrowser("searchPassenger()", "passenger/search", LocalDateTime.now());
-        return passengerService.searchPassenger(toSearch);
+    public List<Passenger> searchPassenger(@RequestParam String toSearch) {
+        return performOperation("searchPassenger()", "passenger/search", () -> passengerService.searchPassenger(toSearch));
     }
 
     @PostMapping("/passenger/addPassenger")
-    public void addPassenger(@RequestBody Passenger passenger){
-        activityService.addActivity("passenger", "create", Map.of("id", passenger.getId(), "firstname",  passenger.getFirstname(), "lastName", passenger.getLastName(), "phoneNumber", passenger.getPhoneNumber(), "aircraftIdsList", passenger.getAircraftIdsList(), "airportIdsList", passenger.getAirportIdsList()));
-        browserService.addToBrowser("addPassenger()", "/passenger/addPassenger", LocalDateTime.now());
-        passengerService.addPassenger(passenger);
+    public void addPassenger(@RequestBody Passenger passenger) {
+        performOperationWithActivity("addPassenger()", "/passenger/addPassenger", passenger,
+                () -> passengerService.addPassenger(passenger));
     }
 
     @DeleteMapping("/passenger/deletePassenger/{id}")
     public List<Passenger> deletePassengerById(@PathVariable int id) {
-        Passenger passengerForActivity = new Passenger();
-        List<Passenger> passengerlist = passengerService.getAllPassenger();
-        for (Passenger passenger : passengerlist){
-            if (passenger.getId() == id) {
-                passengerForActivity = passenger;
-            }
-        }
-        if (passengerForActivity != null) {
-            activityService.addActivity("passenger", "delete", Map.of("id", passengerForActivity.getId(), "firstname",  passengerForActivity.getFirstname(), "lastName", passengerForActivity.getLastName(), "phoneNumber", passengerForActivity.getPhoneNumber(), "aircraftIdsList", passengerForActivity.getAircraftIdsList(), "airportIdsList", passengerForActivity.getAirportIdsList()));
-        }
-
-        String url = "/passenger/deletePassenger/" + String.valueOf(id);
-        browserService.addToBrowser("deletePassenger()", url, LocalDateTime.now());
-        return passengerService.deletePassengerById(id);
+        String url = "/passenger/deletePassenger/" + id;
+        return performOperationWithActivity("deletePassenger()", url, id, () -> passengerService.deletePassengerById(id));
     }
 
     @PutMapping("/passenger/updatePassenger/{id}")
-    public List<Passenger> updatePassenger(@PathVariable int id, @RequestBody Passenger passenger){
-        Passenger passengerForActivity = new Passenger();
-        List<Passenger> passengerlist = passengerService.getAllPassenger();
-        for (Passenger passengerToFind : passengerlist){
-            if (passengerToFind.getId() == id) {
-                passengerForActivity = passengerToFind;
-            }
-        }
-        if (passengerForActivity != null) {
-            activityService.addActivity("passenger", "update", Map.of("id", passengerForActivity.getId(), "firstname",  passengerForActivity.getFirstname(), "lastName", passengerForActivity.getLastName(), "phoneNumber", passengerForActivity.getPhoneNumber(), "aircraftIdsList", passengerForActivity.getAircraftIdsList(), "airportIdsList", passengerForActivity.getAirportIdsList()));
-        }
-
-        String url = "/passenger/updatePassenger/" + String.valueOf(id);
-        browserService.addToBrowser("updatePassenger()", url, LocalDateTime.now());
-        return passengerService.updatePassenger(id, passenger);
+    public List<Passenger> updatePassenger(@PathVariable int id, @RequestBody Passenger passenger) {
+        String url = "/passenger/updatePassenger/" + id;
+        return performOperationWithActivity("updatePassenger()", url, id,
+                () -> passengerService.updatePassenger(id, passenger));
     }
 
     @GetMapping("/passenger/{id}/getAircraft")
     public List<Aircraft> getAircraftPassengerTravelledOn(@PathVariable int id) {
-        String url = "/passenger/"  + String.valueOf(id) + "/getAircraft";
-        browserService.addToBrowser("getAircraft()", url, LocalDateTime.now());
-        return passengerService.getAircraft(id);
+        String url = "/passenger/" + id + "/getAircraft";
+        return performOperation("getAircraft()", url, () -> passengerService.getAircraft(id));
     }
 
     @GetMapping("/passenger/{id}/getAirport")
     public List<Airport> getAirportPassengerVisited(@PathVariable int id) {
-        String url = "/passenger/"  + String.valueOf(id) + "/getAirport";
-        browserService.addToBrowser("getAirport()", url, LocalDateTime.now());
-        return passengerService.getAirports(id);
+        String url = "/passenger/" + id + "/getAirport";
+        return performOperation("getAirport()", url, () -> passengerService.getAirports(id));
+    }
+
+    private <T> T performOperation(String operationName, String url, Operation<T> operation) {
+        browserService.addToBrowser(operationName, url, LocalDateTime.now());
+        return operation.perform();
+    }
+
+    private <T, P> T performOperationWithActivity(String operationName, String url, P parameter, Operation<T> operation) {
+        activityService.addActivity("passenger", operationName, Map.of("id", parameter));
+        return performOperation(operationName, url, operation);
+    }
+
+    private interface Operation<T> {
+        T perform();
     }
 }
