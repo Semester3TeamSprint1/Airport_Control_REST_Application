db2db5cec97f9673ed263a0f287b899ce1bf5f5f
ChambersJarod <Jarod.Chambers-Genge@keyin.com>
2023-07-02 23:15:41 -0230

    browser folder

--- Sem4_Sprint1Two_Main/src/main/java/com/keyin/browser/BrowserController.java
+++ Sem4_Sprint1Two_Main/src/main/java/com/keyin/browser/BrowserController.java
@@ -1,18 +1,55 @@
+//package com.keyin.browser;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//import java.util.List;
+//
+//@RestController
+//@CrossOrigin
+//public class BrowserController {
+//    @Autowired
+//    private BrowserService browserService;
+//
+//    @GetMapping("/browser")
+//    public Browser getPeekBrowser() {
+//        return browserService.getPeekBrowser();
+//    }
+//
+//    @GetMapping("/allBrowserData")
+//    public List<Browser> getAllBrowserData(){
+//        return browserService.getBrowserData();
+//    }
+//
+//    @GetMapping("/undoBrowser")
+//    public Browser undoBrowser(){
+//        return browserService.undoBrowser();
+//    }
+//
+//    @GetMapping("/redoBrowser")
+//    public Browser redoBrowser(){
+//        return browserService.redoBrowser();
+//    }
+//}
 package com.keyin.browser;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin
 public class BrowserController {
+    private final BrowserService browserService;
+
     @Autowired
-    private BrowserService browserService;
+    public BrowserController(BrowserService browserService) {
+        this.browserService = browserService;
+    }
 
     @GetMapping("/browser")
     public Browser getPeekBrowser() {
@@ -20,17 +57,17 @@ public class BrowserController {
     }
 
     @GetMapping("/allBrowserData")
-    public List<Browser> getAllBrowserData(){
+    public List<Browser> getAllBrowserData() {
         return browserService.getBrowserData();
     }
 
     @GetMapping("/undoBrowser")
-    public Browser undoBrowser(){
+    public Browser undoBrowser() {
         return browserService.undoBrowser();
     }
 
     @GetMapping("/redoBrowser")
-    public Browser redoBrowser(){
+    public Browser redoBrowser() {
         return browserService.redoBrowser();
     }
 }
--- Sem4_Sprint1Two_Main/src/main/java/com/keyin/browser/BrowserService.java
+++ Sem4_Sprint1Two_Main/src/main/java/com/keyin/browser/BrowserService.java
@@ -1,18 +1,77 @@
+//package com.keyin.browser;
+//
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDateTime;
+//import java.util.ArrayList;
+//import java.util.Iterator;
+//import java.util.List;
+//import java.util.Stack;
+//
+//@Service
+//public class BrowserService {
+//
+//    private Stack<Browser> browserStack = new Stack<>();
+//    private Stack<Browser> redoStack = new Stack<>();
+//
+//    public void addToBrowser(String calledMethod, String url, LocalDateTime timestamp) {
+//        Browser browser = new Browser(calledMethod, url, timestamp);
+//        browserStack.push(browser);
+//    }
+//
+//    public Browser getPeekBrowser() {
+//        if (browserStack.isEmpty()){
+//            System.out.println("no data present");
+//            return null;
+//        } else {
+//            return browserStack.peek();
+//        }
+//    }
+//
+//    public List<Browser> getBrowserData() {
+//        List<Browser> browserList = new ArrayList<>();
+//        browserList.addAll(browserStack);
+//
+//        if (browserList.isEmpty()){
+//            System.out.println("no data present");
+//            return null;
+//        }
+//        return browserList;
+//    }
+//
+//    public Browser undoBrowser(){
+//        if(browserStack.isEmpty()){
+//            System.out.println("no data present");
+//            return null;
+//        }
+//        redoStack.push(browserStack.peek());
+//        browserStack.pop();
+//        return browserStack.peek();
+//    }
+//
+//    public Browser redoBrowser() {
+//        if (redoStack.isEmpty()){
+//            System.out.println("no data present");
+//        }
+//        browserStack.push(redoStack.peek());
+//        redoStack.pop();
+//        return browserStack.peek();
+//    }
+//}
 package com.keyin.browser;
 
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
 
 @Service
 public class BrowserService {
 
-    private Stack<Browser> browserStack = new Stack<>();
-    private Stack<Browser> redoStack = new Stack<>();
+    private final Stack<Browser> browserStack = new Stack<>();
+    private final Stack<Browser> redoStack = new Stack<>();
 
     public void addToBrowser(String calledMethod, String url, LocalDateTime timestamp) {
         Browser browser = new Browser(calledMethod, url, timestamp);
@@ -20,41 +79,36 @@ public class BrowserService {
     }
 
     public Browser getPeekBrowser() {
-        if (browserStack.isEmpty()){
-            System.out.println("no data present");
+        if (browserStack.isEmpty()) {
+            System.out.println("No data");
             return null;
-        } else {
-            return browserStack.peek();
         }
+        return browserStack.peek();
     }
 
     public List<Browser> getBrowserData() {
-        List<Browser> browserList = new ArrayList<>();
-        browserList.addAll(browserStack);
-
-        if (browserList.isEmpty()){
-            System.out.println("no data present");
+        if (browserStack.isEmpty()) {
+            System.out.println("No data");
             return null;
         }
-        return browserList;
+        return new ArrayList<>(browserStack);
     }
 
-    public Browser undoBrowser(){
-        if(browserStack.isEmpty()){
-            System.out.println("no data present");
+    public Browser undoBrowser() {
+        if (browserStack.isEmpty()) {
+            System.out.println("No data");
             return null;
         }
-        redoStack.push(browserStack.peek());
-        browserStack.pop();
+        redoStack.push(browserStack.pop());
         return browserStack.peek();
     }
 
     public Browser redoBrowser() {
-        if (redoStack.isEmpty()){
-            System.out.println("no data present");
+        if (redoStack.isEmpty()) {
+            System.out.println("No data");
+            return null;
         }
-        browserStack.push(redoStack.peek());
-        redoStack.pop();
+        browserStack.push(redoStack.pop());
         return browserStack.peek();
     }
 }
